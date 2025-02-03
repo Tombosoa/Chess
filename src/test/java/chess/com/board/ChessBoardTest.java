efab36b8c15017563be960c40b8aab7a55cfa0c4
Tombosoa <hei.nantenaina.4@gmail.com>
2025-02-03 21:06:04 +0300

    test: update ChessboardTest and PawnTest

---
+++ src/test/java/chess/com/board/ChessBoardTest.java
@@ -0,0 +1,27 @@
+package chess.com.board;
+
+import com.chess.board.ChessBoard;
+import com.chess.cases.Case;
+import com.chess.enums.AlphabeticalReference;
+import com.chess.enums.NumericalReference;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChessBoardTest {
+    @Test
+    public void getCaseOnBoard(){
+        var bd = new ChessBoard();
+
+        var actualCase = bd.getCase(7, 7);
+        var expectedCase = new Case(NumericalReference.EIGHT, AlphabeticalReference.h);
+
+        assertEquals(expectedCase, actualCase);
+    }
+
+    @Test
+    public void test(){
+        System.out.println(AlphabeticalReference.h.ordinal());
+        System.out.println(NumericalReference.EIGHT.getValue());
+    }
+}
---
+++ src/test/java/chess/com/pieces/PawnTest.java
@@ -0,0 +1,44 @@
+package chess.com.pieces;
+
+import com.chess.cases.Case;
+import com.chess.enums.AlphabeticalReference;
+import com.chess.enums.Color;
+import com.chess.enums.NumericalReference;
+import com.chess.pieces.Pawn;
+import com.chess.pieces.Position;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PawnTest {
+    @Test
+    public void getColAndRow(){
+        var cases = new Case(NumericalReference.TWO, AlphabeticalReference.h);
+
+        var position = new Position(cases);
+
+        var pawn = new Pawn(Color.white, position);
+
+        var col = pawn.getPosition().getCurrentPosition().getCol();
+        var row = pawn.getPosition().getCurrentPosition().getRow();
+
+        assertEquals(7, col);
+        assertEquals(2, row);
+    }
+
+    @Test
+    public void getPossiblesMoves(){
+        var cases = new Case(NumericalReference.TWO, AlphabeticalReference.h);
+
+        var position = new Position(cases);
+
+        var pawn = new Pawn(Color.white, position);
+        pawn.getPosition().getCurrentPosition().setBusy(true);
+        var expectedCases = new Case(NumericalReference.THREE, AlphabeticalReference.h);
+
+        assertTrue(pawn.getPossibleMove(position.getCurrentPosition()).contains(expectedCases));
+
+        System.out.println(pawn.getPosition().getCurrentPosition().isBusy());
+    }
+}
